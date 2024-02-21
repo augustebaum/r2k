5e6f54fb4f04c6425e1872db78c56ac7285554ff
Auguste Baum <auguste.apple@gmail.com>
2024-02-21 10:04:02 +0100

    Use pathlib

--- r2k/constants.py
+++ r2k/constants.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from os.path import dirname, expanduser, join
+from pathlib import Path
+from os.path import dirname, expanduser
 
 CONFIG_ENV_VAR = "RSS_TO_KINDLE_CONFIG"
 
@@ -7,12 +8,12 @@ HTML_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"
 }
 
-DEFAULT_APP_PATH = expanduser("~/.r2k")
-DEFAULT_CONFIG_PATH = join(DEFAULT_APP_PATH, "config.yml")
+DEFAULT_APP_PATH = Path(expanduser("~/.r2k"))
+DEFAULT_CONFIG_PATH = DEFAULT_APP_PATH / "config.json"
 
-PACKAGE_DIR = dirname(__file__)
-TOP_LEVEL_DIR = dirname(PACKAGE_DIR)
-TEMPLATES_DIR = join(TOP_LEVEL_DIR, "templates")
+PACKAGE_DIR = Path(dirname(__file__))
+TOP_LEVEL_DIR = PACKAGE_DIR.parent
+TEMPLATES_DIR = TOP_LEVEL_DIR / "templates"
 
 # Number of articles to put in a single EPUB eBook. Otherwise the email size might exceed GMAIL's 25MB limit
 ARTICLE_EBOOK_LIMIT = 20
