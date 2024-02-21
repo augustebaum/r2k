603c8580f543eaf28f0df98a3e25d2b9634a47c0
Auguste Baum <auguste.apple@gmail.com>
2024-02-21 10:04:38 +0100

    Add test

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,2 +1,19 @@
-def test_sanity():
-    pass
+from r2k.config import Config
+
+
+def test_read_config(tmp_path):
+    """Config can be saved and read."""
+    config_contents = """
+    {
+        "kindle_address": "test@kindle.com",
+        "send_from": "test@gmail.com"
+    }
+    """
+    config_file = tmp_path / "config.yml"
+    config_file.write_text(config_contents)
+
+    config = Config()
+    config.load(config_file)
+
+    assert config.kindle_address == "test@kindle.com"
+    assert config.send_from == "test@gmail.com"
